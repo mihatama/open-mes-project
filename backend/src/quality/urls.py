26c7820e9649dd694e20b461f85b9308ae91c459
yururi-nonbiry <yururi.nonbiry@gmail.com>
2025-07-17 19:34:19 +0900

    品質管理マスター登録

--- backend/src/quality/urls.py
+++ backend/src/quality/urls.py
@@ -4,13 +4,13 @@ from . import views
 app_name = 'quality'
 
 urlpatterns = [
-    # Existing URLs
     path('menu/', views.QualityMenuView.as_view(), name="menu"), # Changed name for consistency
     path('process_inspection/', views.ProcessInspectionView.as_view(), name="process_inspection"),
     path('acceptance_inspection/', views.AcceptanceInspectionView.as_view(), name="acceptance_inspection"),
 
     # Inspection Item Master URLs
-    path('master_creation/', views.InspectionItemMasterView.as_view(), name='inspection_item_master_list'),
+    path('master_creation/', views.InspectionItemMasterView.as_view(), name='master_creation'),
+    path('master_creation/list/', views.inspection_item_create, name='inspection_item_list'), # Add this line
     path('master_creation/create/', views.inspection_item_create, name='inspection_item_create'),
     path('master_creation/update/<uuid:pk>/', views.inspection_item_update, name='inspection_item_update'),
     path('master_creation/delete/<uuid:pk>/', views.inspection_item_delete, name='inspection_item_delete'),
